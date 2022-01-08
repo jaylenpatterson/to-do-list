dabcad0026fd5c3c724752bfd5af1e2b98ffff9d
Dickson Yu <dickson.yu@email.com>
2022-01-08 16:49:46 -0500

    Adding the function of registering a user, adding it into the DB

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const { addUser } = require('./helperFunctions')
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -23,3 +25,19 @@ module.exports = (db) => {
   });
   return router;
 };
+
+// Registration Users
+router.post('/', req, body => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  addUser(user, db)
+  .then(user => {
+    if(!user) {
+      res.send({error: 'Error'});
+      return;
+    }
+    req.session.userId = user.id;
+    res.send('Logged in');
+  })
+  .catch(err => res.send(err));
+});
