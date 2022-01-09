284c0117313e5e48356c0c8f5d9204a2de0e7b44
Dickson Yu <dickson.yu@email.com>
2022-01-08 23:22:14 -0500

    Adding register function

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@
 const express = require('express');
 const router  = express.Router();
 const bcrypt = require('bcrypt');
-const { addUser } = require('./helperFunctions')
+const { addUser } = require('./helperFunctions');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -23,21 +23,22 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // Registration Users
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    addUser(user, db)
+    .then(user => {
+      if(!user) {
+        res.send({error: 'Error'});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send('Logged in');
+    })
+    .catch(err => res.send(err));
+  });
+
   return router;
 };
-
-// Registration Users
-router.post('/', req, body => {
-  const user = req.body;
-  user.password = bcrypt.hashSync(user.password, 12);
-  addUser(user, db)
-  .then(user => {
-    if(!user) {
-      res.send({error: 'Error'});
-      return;
-    }
-    req.session.userId = user.id;
-    res.send('Logged in');
-  })
-  .catch(err => res.send(err));
-});
