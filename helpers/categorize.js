398fbe3b342d2f4def4a3d1cb35703fca45aaf42
jaylenpatterson <jaylenpatterson16@gmail.com>
2022-01-09 03:30:11 +0000

    created functions that work together to search for a key word and determine whether that word corresponds to a movie, book, resturant or a product

--- helpers/categorize.js
+++ helpers/categorize.js
@@ -1,32 +1,5 @@
 const axios = require('axios');
 
-// Produces a string with information about the type of medium/entity the query is
-const searcher = function(query) {
-	const params = {
-		//These API calls are limited to 100 be careful :)
-		api_key: '8E18BC37CDA44180A962F38036260267',
-		engine: 'google',
-		gl: 'ca',
-		lr: 'lang_en',
-		cr: 'ca',
-		q: query
-	};
-
-	// make the http GET request to SerpWow
-	axios
-		.get('https://api.serpwow.com/search', { params })
-		.then((response) => {
-			// print the JSON response from SerpWow
-			console.log(JSON.stringify(response.data['organic_results'][0]['snippet'], 0, 2));
-		})
-		.catch((error) => {
-			// catch and print the error
-			console.log(error);
-		});
-};
-
-searcher('churches chicken');
-
 // keyWords for regex
 
 const watchKeyWords = [
@@ -46,7 +19,9 @@ const watchKeyWords = [
 	'stream',
 	'season',
 	'Blu-ray',
-	'director'
+	'director',
+	'show',
+	'TV'
 ];
 const readKeyWords = [
 	'novel',
@@ -62,9 +37,9 @@ const readKeyWords = [
 	'poem',
 	'poems',
 	'volume',
-  'vol',
-  'vols',
-  'library'
+	'vol',
+	'vols',
+	'library'
 ];
 const eatKeyWords = [
 	'meal',
@@ -84,6 +59,88 @@ const eatKeyWords = [
 	'recipe',
 	'marinate'
 ];
+
+const findWatchKeyWords = function(str) {
+	for (let word of watchKeyWords) {
+		if (str.search(word) != -1) {
+			return true;
+		}
+	}
+	return false;
+};
+
+const findReadKeyWords = function(str) {
+	for (let word of readKeyWords) {
+		if (str.search(word) != -1) {
+			return true;
+		}
+	}
+	return false;
+};
+
+const findEatKeyWords = function(str) {
+	for (let word of eatKeyWords) {
+		if (str.search(word) != -1) {
+			return true;
+		}
+	}
+	return false;
+};
+
+// Produces a string with information about the type of medium/entity the query is
+const searcher = function(query) {
+	const params = {
+		//These API calls are limited to 100 be careful :)
+		api_key: '8E18BC37CDA44180A962F38036260267',
+		engine: 'google',
+		gl: 'ca',
+		lr: 'lang_en',
+		cr: 'ca',
+		q: query
+	};
+
+	// make the http GET request to SerpWow
+	axios
+		.get('https://api.serpwow.com/search', { params })
+		.then((response) => {
+			// console.log(JSON.stringify(response.data['organic_results'][0]['snippet']))
+			// determines if any key words can be found within the string that the searcher function retreives. If so return true otherwise return false
+			if (findWatchKeyWords(JSON.stringify(response.data['organic_results'][0]['snippet'], 0, 2))) {
+				return "watch";
+			}
+
+			if (findReadKeyWords(JSON.stringify(response.data['organic_results'][0]['snippet'], 0, 2))) {
+				return "read";
+			}
+
+			if (findEatKeyWords(JSON.stringify(response.data['organic_results'][0]['snippet'], 0, 2))) {
+				return "resturant";
+			} else {
+        return "buy";
+			}
+		})
+		.catch((error) => {
+			// catch and print the error
+			console.log(error);
+		});
+};
+
+// searcher('harry potter');
+
+// module.exports(searcher);
+
+
+// let re = new RegExp(//, 'i')
+
+// const findWatchKeyWords = function(str) {
+//   for(let word of watchKeyWords) {
+//     if (str.search(word) != -1) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// findWatchKeyWords("family guy")
 
 // knowledge_graph.description (for resturants)
 // knowledge_graph.type (for movies and books)
