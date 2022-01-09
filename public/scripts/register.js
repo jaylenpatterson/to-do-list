6f37df7b0fa3637560ca57be0388ff0647f46c16
Dickson Yu <dickson.yu@email.com>
2022-01-08 23:26:18 -0500

    Adding function for regsitering users

---
+++ public/scripts/register.js
@@ -0,0 +1,21 @@
+$(() => {
+  $(document).on('submit', '#register-form', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    register(serializedData);
+  });
+});
+
+// Create new header when the user registers
+const register = function(data) {
+  $.ajax({
+    method: 'POST',
+    url: '/users/',
+    data: data
+  })
+  .then(data => {
+    console.log(data);
+    createNewHeaderDiv(data);
+    appendMain(data);
+  });
+};
