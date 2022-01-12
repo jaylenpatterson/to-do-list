217565f757bcf8bbf041070c3bddc88cfac12808
Dickson Yu <dickson.yu@email.com>
2022-01-11 21:50:59 -0500

    Adding test schema and seeds, and updated .json

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -3,6 +3,6 @@
 DROP TABLE IF EXISTS widgets CASCADE;
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
+  widget_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL
 );
