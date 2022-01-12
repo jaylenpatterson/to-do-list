01299ba6f0771c1f9ad76f84cee41246efbc2094
Dickson Yu <dickson.yu@email.com>
2022-01-11 21:10:37 -0500

    Fixing a typo

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const taskRoutes = require("./routes/tasks");
+const taskRoutes = require("./routes/task");
 const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use('/users', usersRoutes(db));
-app.use('/tasks', taskRoutes(db));
+app.use('/task', taskRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
