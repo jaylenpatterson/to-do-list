0c5288e2d20da2c4c6c036b2a7d29e9dd2fc81fb
Dickson Yu <dickson.yu@email.com>
2022-01-11 20:59:20 -0500

    Adding helper function, to get user tasks

--- routes/helperFunctions.js
+++ routes/helperFunctions.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 // Insert new registered user into the database
-const addUser = function(user, db) {
+const addUser = (user, db) => {
   const queryString = `
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
@@ -14,6 +14,23 @@ const addUser = function(user, db) {
   .catch(err => console.log('Query Error:', err));
 };
 
+
+// Get tasks that based on user
+const getUsersTasks = function(userid, db) {
+  const queryString = `
+		SELECT *
+  	FROM tasks
+  	WHERE tasks.user_id = $1
+  	ORDER BY tasks.date_created ASC;`;
+  const value = [userid];
+  return db.query(queryString, value)
+    .then(res => res.rows)
+    .catch(err => {
+      console.error('query error', err.stack);
+    });
+};
+
 module.exports = {
-  addUser
+  addUser,
+  getUsersTasks
 };
