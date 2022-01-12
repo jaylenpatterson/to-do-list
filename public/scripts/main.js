df41d96283ba6ca8203b33498da3d3f8a89157cd
Dickson Yu <dickson.yu@email.com>
2022-01-11 20:54:29 -0500

    Adding a way to render the tasks based on user

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $main = $("main");
+  getTheUser()
+    .then(userData => {
+      appendMain(userData);
+    });
+});
+
+// Shows main page depending on if user is logged in or not
+const appendMain = function(user) {
+  $main = $("main");
+  $main.empty();
+  if (Object.keys(user).length > 0) {
+    renderTasks();
+  }
+};
+
+
+
+// HTML content to be dynamically added
+
