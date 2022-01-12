99ee24e16b0c9d4d86d2258f8646db43363a37ac
Dickson Yu <dickson.yu@email.com>
2022-01-12 18:05:37 -0500

    Adding the function of submitting the form and add to database

--- public/scripts/tasks.js
+++ public/scripts/tasks.js
@@ -1,15 +1,17 @@
 $(function() {
+  let clickDisabled = false;
 
+  submitTask();
 });
 
 // Creates an HTML task element
 const createTaskElement = (taskObj) => {
   let $newTask = null;
+  const $task = $('<article>').addClass('listed-tasks');
 
     $newTask = `
-    <article class="listed-tasks">
     <header>
-      <h4>${taskObj.title}</h4>
+      <h4>${taskObj.description}</h4>
       <span class="icons">
         <i class="fa-solid fa-info fa-fw"></i>
         <i class="fa-solid fa-pen-to-square fa-fw"></i>
@@ -21,29 +23,21 @@ const createTaskElement = (taskObj) => {
     </article>
     </article>
     `;
-  return $newTask;
+
+    $task.append($newTask);
+    return $task;
 };
 
 // Function to assist to clear the container before rendering
 const clearTasks = () => {
-  $(".watch.taskContainer").empty();
-  $(".eat.taskContainer").empty();
-  $(".read.taskContainer").empty();
-  $(".buy.taskContainer").empty();
-  $(".null.taskContainer").empty();
 
-  $('.h2.watch').hide();
-  $('.h2.eat').hide();
-  $('.h2.read').hide();
-  $('.h2.buy').hide();
-  $('.h2.null').hide();
 };
 
 
 // Function to create a list of task for rendering
 const taskListBuilder = (task) => {
   const newTask = createTaskElement(task);
-    $('main').prepend(newTask);
+    $('main').append(newTask);
 };
 
 // Function that grabs the tasks from the database and renders them
@@ -56,4 +50,44 @@ const renderTasks = () => {
           taskListBuilder(task);
       }
     });
+};
+
+// function that assists when a new task is submitted
+const submitTask = () => {
+
+  let clickDisabled = false;
+
+  $(document).on('submit', '#new-task-form', (event) => {
+    event.preventDefault();
+    if (clickDisabled) {
+      return;
+    }
+    clickDisabled = true;
+    setTimeout(function() {
+      clickDisabled = false;
+    },2000);
+
+    const input = $('#new-task-form');
+    const textObj = input.find('#task-description');
+
+    const serializedText = textObj.serialize();
+    const textValue = textObj.val();
+
+    const error = input.find('#error');
+    const errorIcon = `<i class="fas fa-exclamation-triangle"></i>`;
+    error.html("");
+    // client-side error checking prior to posting the value
+    if (textValue === "" || textValue === null) {
+      error.append(`${errorIcon}  Error: task description cannot be empty`);
+    } else {
+      const modal = $('#new-task-popup');
+      $.post('/task', serializedText)
+        .then(() => {
+          $('#new-task-popup').fadeOut(500);
+          (modal.toggleClass('show'));
+          textObj.val('');
+          renderTasks();
+        });
+    }
+  });
 };
