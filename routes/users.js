a78653601972a2605beb00d3519acd369c11e141
Dickson Yu <dickson.yu@email.com>
2022-01-11 20:58:01 -0500

    Adjusting spacing

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const bcrypt = require('bcrypt');
 const { addUser } = require('./helperFunctions');
 
