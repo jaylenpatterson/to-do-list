aec3af8421a4152ebb4d7a39af0d9abbc07c0fbc
Dickson Yu <dickson.yu@email.com>
2022-01-11 22:05:59 -0500

    Fixing merge conflicts

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,24 +1,11 @@
 -- Drop and recreate Users table (Example)
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS tasks CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE tasks (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  start_date DATE,
-  category VARCHAR(255),
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  urgency SMALLINT DEFAULT 1,
-  complete BOOLEAN NOT NULL DEFAULT FALSE
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
 );
 
 
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, password, email) VALUES ('Alice', 'test', 'test@test.com');
+INSERT INTO users (name, password, email) VALUES ('Kira', 'test', 'test2@test.com');
