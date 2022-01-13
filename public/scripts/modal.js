228d2ebff9129da90d209b7e659c87684c961f5d
Dickson Yu <dickson.yu@email.com>
2022-01-13 02:01:50 -0500

    Adding modal window

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -4,12 +4,10 @@ $(document).ready(() => {
 });
 
 const submitModal = function() {
-  // Get the button that opens the modal
   $(document).on('click',"#new-task", () => {
     $('#new-task-popup').fadeIn(500);
     $('#new-task-form').find('#task-description').focus();
   });
-  // Get the span element that closes the modal
   $(document).on('click','.close.new', () => {
     $('#new-task-popup').fadeOut(500);
   });
@@ -17,6 +15,15 @@ const submitModal = function() {
 
 
 const editModal = function() {
+  // Opens the modal window for editing the task
+	$(document).on('click', '.edit-task', function() {
+  	$('#edit-task-popup').fadeIn(500);
+    $('#edit-task-form').find('#edit-task-description').focus();
+  	setTimeout(() => {
+    	$('#edit-task-popup').toggleClass('show')
+  	}, 500);
+	});
+
 	// Close modal
 	$(document).on('click', '.close.edit', (event) => {
   	$('#edit-task-popup').fadeOut(500);
