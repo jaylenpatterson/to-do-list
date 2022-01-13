f516aa5fdb74a2c8742035db321b1d1df1559cc3
Dickson Yu <dickson.yu@email.com>
2022-01-13 02:00:38 -0500

    Adding edit form in dynamic html

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,6 +12,7 @@ const appendMain = function(user) {
   $main.empty();
   if (Object.keys(user).length > 0) {
     $('main').prepend($newTaskModal);
+    $('main').append($editTaskModal);
     $('main').append($buttonGroup);
     $('main').append($newTask);
     $('main').append($taskContainer);
@@ -44,7 +45,7 @@ const $taskContainer = `
 </div>
 </section>
 `;
-const $newTask = `<button id="new-task">Add a new task</button>`;
+const $newTask = `<button id="new-task">New Task</button>`;
 
 const $buttonGroup = `
 <div class="button-group">
@@ -55,14 +56,14 @@ const $buttonGroup = `
   <button class='read-category-btn'><i class="fas fa-book"></i></button>
   <button class='buy-category-btn'><i class="fas fa-shopping-cart"></i></button>
   <button class='completed-category-btn'><i class="fas fa-check-square"></i></button>
-</div>`;
+</div>
+`;
 
 const $newTaskModal = `
 <!-- The Modal -->
 <div id="new-task-popup" class="modal">
   <!-- Modal content -->
   <div class="modal-content">
-    <span class="close new">&times;</span>
     <form id="new-task-form">
       <div>
         <textarea name="text_description" id="task-description" placeholder="Enter task description"></textarea>
@@ -71,4 +72,28 @@ const $newTaskModal = `
       </div>
     </form>
   </div>
-</div>`;
+</div>
+`;
+
+const $editTaskModal = `
+<div id="edit-task-popup" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <form id="edit-task-form">
+      <div>
+        <label id="category-modal" for="category">Category:</label>
+        <select class="category-select" name="category">
+          <option value="null">Uncategorized</option>
+          <option value="watch">To Watch</option>
+          <option value="eat">To Eat</option>
+          <option value="read">To Read</option>
+          <option value="buy">To Buy</option>
+        </select>
+        <textarea name="text_description" id='edit-task-description' placeholder="New Description (optional)"></textarea>
+        <button type="submit">Submit</button>
+        <div class="error"></div>
+      </div>
+    </form>
+  </div>
+</div>
+`;
