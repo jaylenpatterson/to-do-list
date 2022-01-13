7ac72ee6eb012dc8fceccc6a0fcba7a2378c4f0f
Dickson Yu <dickson.yu@email.com>
2022-01-13 13:05:33 -0500

    Adding grabbing task ID

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -8,27 +8,27 @@ const submitModal = function() {
     $('#new-task-popup').fadeIn(500);
     $('#new-task-form').find('#task-description').focus();
   });
-  $(document).on('click','.close.new', () => {
+  $(document).on('click','.close.newTask', () => {
     $('#new-task-popup').fadeOut(500);
   });
 };
 
-
 const editModal = function() {
   // Opens the modal window for editing the task
 	$(document).on('click', '.edit-task', function() {
+
+  	const taskID = $(this).closest('.edit-task').attr('id')
+
   	$('#edit-task-popup').fadeIn(500);
     $('#edit-task-form').find('#edit-task-description').focus();
   	setTimeout(() => {
     	$('#edit-task-popup').toggleClass('show')
   	}, 500);
+    $('body').data('taskID', taskID)
 	});
 
 	// Close modal
-	$(document).on('click', '.close.edit', (event) => {
+	$(document).on('click', '.close.editTask', () => {
   	$('#edit-task-popup').fadeOut(500);
-  	setTimeout(() => {
-    	$('#edit-task-popup').toggleClass('show');
-  	}, 500);
 	});
 }
